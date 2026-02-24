b9dab285477b775348dd379502e50d24e039a39d
AlexFafonkin <alexfaf@rambler.ru>
2026-02-24 15:13:14 +0300

    grammar fix and adding new goods

--- postgres/init/insertion.sql
+++ postgres/init/insertion.sql
@@ -1,4 +1,4 @@
 INSERT INTO products (name, price, image_url, description, stock_quantity) VALUES
-('Джинсы Slim Fit', 5000, '/images/goods/jeans.webp', 'Синие джинсы', 50),
+('Джинсы Slim Fit', 5000, '/images/goods/jeans.webp', 'Коричневые джинсы', 50),
 ('Кроссовки Nike', 7000, '/images/goods/nike.webp', 'Легкие беговые кроссовки', 40),
 ('Кросcовки Puma', 5000, '/images/goods/puma.webp', 'Городской рюкзак', 30);
